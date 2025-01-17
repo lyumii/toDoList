facdfafde3a3f960fbb154f1bcd733ae0c599af3
lyumii <dmly1124@gmail.com>
2025-01-17 22:36:34 +0100

    added all core functions i think

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ const nameInput = document.getElementById("name-input");
 const descriptionInput = document.getElementById("description-input");
 const addBtn = document.getElementById("add-button");
 const incompleteList = document.getElementById("incomplete-list");
+const completeList = document.getElementById("complete-list");
+const completeCheck = document.getElementsByClassName("complete-check");
 
 let incompleteToDos = [];
 let completeToDos = [];
@@ -16,6 +18,35 @@ class ToDo {
 }
 
 addBtn.addEventListener("click", () => {
+  addToDo();
+});
+
+addBtn.addEventListener("keydown", (event) => {
+  if (event.key === "Enter") {
+    event.preventDefault();
+    addToDo();
+  }
+});
+
+incompleteList.addEventListener("click", (event) => {
+  if (event.target.matches(".complete-check")) {
+    const li = event.target.closest("li");
+    locateToDo(event);
+  }
+});
+
+function locateToDo(event) {
+  const checkedBox = event.target;
+  const index = checkedBox.dataset.index;
+  const todo = incompleteToDos[index];
+  todo.isComplete = true;
+  completeToDos.push(incompleteToDos.splice(index, 1)[0]);
+  renderCompletedToDos();
+}
+
+/// remove the pushed todo from the ol
+
+function addToDo() {
   if (nameInput.value) {
     incompleteToDos.push(new ToDo(nameInput.value, descriptionInput.value));
     nameInput.value = "";
@@ -24,15 +55,24 @@ addBtn.addEventListener("click", () => {
     alert("Please provide a valid task name.");
   }
   renderToDos();
-  console.log(incompleteToDos);
-});
+}
 
+/// make todo description appear only if the string is not empty
 function renderToDos() {
   incompleteList.innerHTML = "";
   for (let i = 0; i < incompleteToDos.length; i++) {
     let li = document.createElement("li");
-    li.innerHTML = `ToDo Name: ${incompleteToDos[i].name} <input type="checkbox" id="complete-check"> <br>
+    li.innerHTML = `ToDo Name: ${incompleteToDos[i].name} <input type="checkbox" class="complete-check" data-index="${i}"> <br>
                     ToDo Description: ${incompleteToDos[i].description}`;
     incompleteList.appendChild(li);
+  }
+}
+
+function renderCompletedToDos() {
+  completeList.innerHTML = "";
+  for (let i = 0; i < completeToDos.length; i++) {
+    let li = document.createElement("li");
+    li.innerHTML = `Completed ${completeToDos[i].name} at date/time`;
+    completeList.appendChild(li);
   }
 }
